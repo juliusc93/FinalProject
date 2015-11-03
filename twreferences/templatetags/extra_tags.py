d130bcecb519f705e5014a1da6a0c2d39ca14af9
Julio <julio@example.com>
2015-11-03 13:14:57 -0500

    custom tags for better output

---
+++ twreferences/templatetags/extra_tags.py
@@ -0,0 +1,8 @@
+from django import template
+from django.template.defaultfilters import urlize
+
+register = template.Library()
+
+@register.filter(needs_autoescape=True)
+def urlize_list(array, autoescape=True):
+	return [urlize(elem, autoescape=autoescape).encode("utf-8") for elem in array]
